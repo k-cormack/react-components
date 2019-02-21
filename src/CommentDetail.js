64a695243a6dcf0fa582bac8500e7dcb36e04328
Keith Cormack <kcormack@definitivelogic.com>
2019-02-20 17:56:28 -0700

    props2 - content,image

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import faker from 'faker';
+// import faker from 'faker';
 
 const CommentDetail = (props) => {
   return (
     <div className="comment">
     <a href="/" className="avatar">
-      <img alt="avatar" src={faker.image.avatar()} />
+      <img alt="avatar" src={props.imgSrc} />
     </a>
     <div className="content">
     <a href="/" className="author">
       {props.author}
     </a>
     <div className="metadata">
-    <span className="date">Today at 6:00PM</span>
+    <span className="date">{props.timeAgo}</span>
     </div>
-    <div className="text">Nice BLOG Post!</div>
+    <div className="text">{props.content}</div>
     </div>
   </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import faker from 'faker';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />  {/* do NOT use the curly-braces! */}
-      <CommentDetail author="Alex" />  {/* do NOT use the curly-braces! */}
-      <CommentDetail author="Jane" />  {/* do NOT use the curly-braces! */}
+      <CommentDetail
+        author="Sam" 
+        timeAgo="Today at 4:45PM"
+        content="NICE Blog1!!!!"
+        imgSrc={faker.image.avatar()}
+        />  {/* do NOT use the curly-braces! */}
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00PM"
+        content="NICE Blog2!!!!"
+        imgSrc={faker.image.avatar()}
+        />  {/* do NOT use the curly-braces! */}
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 9:45PM"
+        content="NICE Blog3!!!!"
+        imgSrc={faker.image.avatar()}
+        />  {/* do NOT use the curly-braces! */}
 
     </div>
   );
